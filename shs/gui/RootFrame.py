5a92f7c71a8a08c5e77adc0ec22dcb40543900e4
ansobolev <andrey.n.sobolev@gmail.com>
2014-08-26 20:08:38 +0600

    several adjustments due to PEP 8

--- shs/gui/RootFrame.py
+++ shs/gui/RootFrame.py
@@ -7,22 +7,25 @@ import subprocess
 import wx
 import ConfigParser
 from wx.lib.mixins.listctrl import getListCtrlSelection
-from wx.lib.pubsub import Publisher
+from wx.lib.pubsub import pub
 
 from gui.RootGUI import RootGUI
-import StepsDialog as SD
-import PlotFrame as PF
+from StepsDialog import StepsDialog
+from PlotFrame import PlotFuncFrame, PlotCorrFrame
 import interface
 import mbox
+
 
 class RootFrame(RootGUI):
 
     calcs = []
+    plot_frame = None
 
     def __init__(self, *args, **kwds):
         super(RootFrame, self).__init__(*args, **kwds)
         # set root
         self.root = self.set_root()
+
         # initialize choices
         self.propChoices = interface.dataClasses()
         calc_data_types = self.propChoices.types()
@@ -37,11 +40,11 @@ class RootFrame(RootGUI):
         self.xCorr.SetSelection(0)
         self.yCorr.SetSelection(0)
         # initialize calc tree
-        self.buildTree(self.root,self.typeRBox.GetItemLabel(self.typeRBox.GetSelection()))
+        self.build_tree(self.root, self.typeRBox.GetItemLabel(self.typeRBox.GetSelection()))
         # initialize calc list
-        self.calcList.InsertColumn(0,'Directory', width = 180)
-        self.calcList.InsertColumn(1,'Type', width = 70)
-        self.calcList.InsertColumn(2,'NSteps', width = 100)
+        self.calcList.InsertColumn(0, 'Directory', width=180)
+        self.calcList.InsertColumn(1, 'Type', width=70)
+        self.calcList.InsertColumn(2, 'NSteps', width=100)
 
     def set_root(self):
         """
@@ -72,43 +75,47 @@ class RootFrame(RootGUI):
             config.write(f)
         return root_dir
 
-    def buildTree(self,root,ctype):
-        '''Add a new root element and then its children'''        
+    def build_tree(self, root, calc_type):
+        """
+        Adds a new root element and then its children
+        :param root: root directory for the tree
+        :param calc_type: calculation type
+        """
         self.calcTree.DeleteAllItems()
         r = len(root.split(os.sep))
-        ids = {root : self.calcTree.AddRoot(root)}
-        for (dirpath, dirnames, filenames) in os.walk(root):
-            if interface.isCalcOfType(ctype, dn = dirnames, fn = filenames):
+        ids = {root: self.calcTree.AddRoot(root)}
+        for (dir_path, dir_names, file_names) in os.walk(root):
+            if interface.isCalcOfType(calc_type, dn=dir_names, fn=file_names):
                 # find the number of steps in MDE file, quickly
-                nsteps = interface.GetNumMDESteps(dirpath)
-                ancdirs = dirpath.split('/')[r:]
+                nsteps = interface.GetNumMDESteps(dir_path)
+                ancdirs = dir_path.split('/')[r:]
                 if nsteps is not None:
-                    ancdirs[-1] += ' [%i]' % (nsteps)
+                    ancdirs[-1] += ' [%i]' % nsteps
                 ad = root
                 for ancdir in ancdirs:
                     d = os.path.join(ad, ancdir)
-                    if not ids.has_key(d):
+                    if not d in ids:
                         ids[d] = self.calcTree.AppendItem(ids[ad], ancdir)
                     self.calcTree.SortChildren(ids[ad])
                     ad = d
     
-    def getSelectionDir(self):
-        item =  self.calcTree.GetSelection()
+    def get_selection_dir(self):
+        item = self.calcTree.GetSelection()
         parent = self.calcTree.GetItemParent(item)
         path = [self.calcTree.GetItemText(item)]
         while self.calcTree.GetItemText(parent) != '':
             path.append(self.calcTree.GetItemText(parent))
             parent = self.calcTree.GetItemParent(parent)
 # calculation directory
-        cdir = os.sep.join(path[::-1]).split()[0]
-        return cdir
+        calc_dir = os.sep.join(path[::-1]).split()[0]
+        return calc_dir
     
     def onSelChange(self, event):
 # calculation type        
         ctype = self.typeRBox.GetItemLabel(self.typeRBox.GetSelection())
 # calculation directory
-        cdir = self.getSelectionDir() 
-        if interface.isCalcOfType(ctype, dir = cdir):
+        cdir = self.get_selection_dir() 
+        if interface.isCalcOfType(ctype, dir=cdir):
             self.enqueueBtn.Enable()
         else:
             self.enqueueBtn.Enable(False)
@@ -122,7 +129,7 @@ class RootFrame(RootGUI):
 
     def typeChange(self, event): 
         ctype = self.typeRBox.GetItemLabel(self.typeRBox.GetSelection())
-        self.buildTree(self.root, ctype)
+        self.build_tree(self.root, ctype)
 
     def upBtnPress(self, event):
         # selection indices
@@ -143,15 +150,15 @@ class RootFrame(RootGUI):
 # calculation type        
         ctype = self.typeRBox.GetItemLabel(self.typeRBox.GetSelection())
 # calculation directory
-        cdir = self.getSelectionDir()
-        if not interface.isCalcOfType(ctype, dir = cdir):
+        cdir = self.get_selection_dir()
+        if not interface.isCalcOfType(ctype, dir=cdir):
             mbox.NoResults(cdir, ctype)
             return 1
 # init steps range
         r = None
         if ctype in ('.output', '.ANI'):            
 # enter dialog
-            dlg = SD.StepsDialog(None)
+            dlg = StepsDialog(None)
             if dlg.ShowModal() == wx.ID_OK:
                 r = dlg.GetRange()
             dlg.Destroy()
@@ -161,25 +168,27 @@ class RootFrame(RootGUI):
         self.calcList.SetStringItem(clc, 2, str(len(r)) if r is not None else '')
         return 0
 
-    def enqueuePress(self, event):
+    def on_enqueue_press(self, event):
         from sshutils import getMount, getDevice, getRemoteDir
         # on which device are we?
-        cdir = self.getSelectionDir()
-        mpath = getMount(cdir)
-        devname, devtype = getDevice(mpath)
-        if 'ssh' in devtype:
-            user, hostdir = devname.split('@')
-            hostname, remotempath = hostdir.split(':')
-            remotedir = getRemoteDir(cdir, mpath, remotempath)
-            self.enqueueRemote(remotedir, hostname, user)
+        calc_dir = self.get_selection_dir()
+        mount_path = getMount(calc_dir)
+        device_name, device_type = getDevice(mount_path)
+        if 'ssh' in device_type:
+            user, host_dir = device_name.split('@')
+            hostname, remote_mount_path = host_dir.split(':')
+            remote_dir = getRemoteDir(calc_dir, mount_path, remote_mount_path)
+            self.enqueue_remote(remote_dir, hostname, user)
         else:
-            self.enqueueLocal(cdir)
+            self.enqueue_local(calc_dir)
 
-    def enqueueLocal(self, cdir):
-        '''Enqueue a task on a local filesystem
-        Input:
-         -> cdir: calculation directory on a local filesystem 
-        '''
+    @staticmethod
+    def enqueue_local(calc_dir):
+        """
+        Enqueue a task on a local filesystem
+        :param calc_dir: calculation directory on a local filesystem
+        :return: error_code (0 is OK)
+        """
         import distutils.spawn
         # find which queue system is implemented on cluster (qstat - PBS, sinfo - SLURM)
         if distutils.spawn.find_executable('qstat') is not None:
@@ -191,16 +200,18 @@ class RootFrame(RootGUI):
             return -1
         comm = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', q, q + '.sh'))
 
-        submit = subprocess.Popen(['/bin/bash', comm, '-d=' + cdir], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        submit = subprocess.Popen(['/bin/bash', comm, '-d=' + calc_dir], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         mbox.JobSubmit(q, submit.communicate())
 
-    def enqueueRemote(self, cdir, host, user):
-        '''Enqueue a task on a remote filesystem
-        Input:
-         -> cdir: calculation directory on a remote filesystem 
-         -> host: host where to enqueue a task
-         -> user: user of a remote system who enqueues a task  
-        '''
+    @staticmethod
+    def enqueue_remote(calc_dir, host, user):
+        """
+        Enqueue a task on a remote filesystem
+        :param calc_dir: calculation directory on a remote filesystem
+        :param host: host where to enqueue a task
+        :param user: user of a remote system who enqueues a task
+        :return: error code (0 is OK)
+        """
         from sshutils import getSSHClient, getQueue, copyFile, removeFile, runCommand
         ssh = getSSHClient(host, user)
         # find which queue system is implemented on cluster (qstat - PBS, sinfo - SLURM)
@@ -209,51 +220,50 @@ class RootFrame(RootGUI):
             mbox.JobSubmit(None, ())
             return None
         # queue putter on a local machine
-        localdir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', q))
+        local_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', q))
         putter = q + '.sh'
         
-        sftp = copyFile(ssh, putter, localdir, cdir)
-        remotefile = os.path.join(cdir, putter)
-        stdout, stderr = runCommand(ssh, 'bash ' + remotefile + ' -d=' + cdir)
+        sftp = copyFile(ssh, putter, local_dir, calc_dir)
+        remote_file = os.path.join(calc_dir, putter)
+        stdout, stderr = runCommand(ssh, 'bash ' + remote_file + ' -d=' + calc_dir)
         mbox.JobSubmit(q, ('\n'.join(stdout.readlines()), '\n'.join(stderr.readlines())))
-        removeFile(sftp, remotefile)
+        removeFile(sftp, remote_file)
         ssh.close()
     
     def plotBtnPress(self, event):
         if self.noteBook.GetSelection() == 0:
-            self.plotProperty()
+            self.plot_property()
         else:
-            self.plotCorrelation()
+            self.plot_correlation()
     
-    def plotProperty(self):
+    def plot_property(self):
         # plot options - get all the data to plot
         ptype = self.propType.GetItems()[self.propType.GetSelection()]
         pchoice = self.propChoice.GetItems()[self.propChoice.GetSelection()]
         data_class = self.propChoices.dataClass(ptype, pchoice)
         leg = [self.calcList.GetItemText(i) for i in getListCtrlSelection(self.calcList)]
         t1 = time.clock()
-        plot_data = interface.getData(ptype, data_class, leg, 
-                                       [self.calcs[i] for i in getListCtrlSelection(self.calcList)])        
+        plot_data = interface.getData(ptype, data_class, leg,
+                                      [self.calcs[i] for i in getListCtrlSelection(self.calcList)])
         self.SetStatusText('Calculation time: %7.2f s.' % (time.clock() - t1))
         msg = plot_data
         try:
-            self.pf.Raise()
-        except (AttributeError, wx._core.PyDeadObjectError):
-            self.pf = PF.PlotFuncFrame(self)
-            self.pf.Show()
-        Publisher().sendMessage(('data.plot'), msg)
+            self.plot_frame.Raise()
+        except (AttributeError, wx.PyDeadObjectError):
+            self.plot_frame = PlotFuncFrame(self)
+            self.plot_frame.Show()
+        pub.Publisher().sendMessage(('data.plot'), msg)
 
-    def plotCorrelation(self):
+    def plot_correlation(self):
         # correlate options - get all the data to plot
         xchoice = self.xCorr.GetSelection()
         ychoice = self.yCorr.GetSelection()
         leg = [self.calcList.GetItemText(i) for i in getListCtrlSelection(self.calcList)]
-        t1 = time.clock()
-        data, info = interface.get_corr(xchoice, ychoice, [self.calcs[i] for i in getListCtrlSelection(self.calcList)])
+        data, info = interface.getCorr(xchoice, ychoice, [self.calcs[i] for i in getListCtrlSelection(self.calcList)])
         msg = [leg, data, info]
         try:
-            self.cf.Raise()
-        except (AttributeError, wx._core.PyDeadObjectError):
-            self.cf = PF.PlotCorrFrame(self)
-            self.cf.Show()
-        Publisher().sendMessage(('corr.plot'), msg)
+            self.plot_frame.Raise()
+        except (AttributeError, wx.PyDeadObjectError):
+            self.plot_frame = PlotCorrFrame(self)
+            self.plot_frame.Show()
+        pub.Publisher().sendMessage(('corr.plot'), msg)
